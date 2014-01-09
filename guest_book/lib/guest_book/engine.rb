50922b2b3af463a8900d79fdc64e4ca78d75a8b9
Valentine Ostakh <valentine.ostakh@ecommerce.com>
2014-01-09 16:17:51 +0200

    rspec for guestbook engine

--- guest_book/lib/guest_book/engine.rb
+++ guest_book/lib/guest_book/engine.rb
@@ -1,5 +1,10 @@
 module GuestBook
   class Engine < ::Rails::Engine
     isolate_namespace GuestBook
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+    end
   end
 end

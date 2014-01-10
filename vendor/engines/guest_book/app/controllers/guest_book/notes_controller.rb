66dd13031545aee2d69b8189152ea26a03fb2e04
Valentine Ostakh <valentine.ostakh@ecommerce.com>
2014-01-10 18:59:22 +0200

    form for new note

--- vendor/engines/guest_book/app/controllers/guest_book/notes_controller.rb
+++ vendor/engines/guest_book/app/controllers/guest_book/notes_controller.rb
@@ -4,6 +4,13 @@ module GuestBook
   class NotesController < ApplicationController
     def index
       @notes = Note.recent
+      @new_note = Note.new
+    end
+
+    def create
+      @note = Note.new(params[:note])
+      @note.create
+      redirect_to :index
     end
   end
 end
